5c42df35c28b701016cdb61180a2f642f9f524da
iamswatantra2000 <iamsearchingjobsfordev2025@gmail.com>
2025-01-07 17:30:09 +0530

    update the code of Array09.js

--- Arrays/Array09.js
+++ Arrays/Array09.js
@@ -2,13 +2,12 @@
 // Given two arrays, find the intersection (common elements) between them.
 
 
-const arr1 = [1,2,3,4,5];
-const arr2 = [6,7,7,8,9];
+const arr1 = [1, 6, 3, 4, 5];
+const arr2 = [6, 7, 7, 8, 9];
 
 
-function intersectArrays(){
-    let arr3 = arr1.concat(arr2);
-    return arr3;
+function intersectArrays() {
+    return arr1.filter((val) => arr2.includes(val));
 }
 
 
