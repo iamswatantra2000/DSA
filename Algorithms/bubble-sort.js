3c32564389c4d213881ebe0adf0ce93b07058944
Swatantra Gupta <iamswatantra2000@gmail.com>
2025-01-24 10:01:31 +0530

    delete the sort algorithm file and add a new folder of Linked List

--- Algorithms/bubble-sort.js
+++
@@ -1,8 +0,0 @@
-const arr = [1,5,7,2,3,8,4,6];
-
-
-function BubbleSort(arr){
-
-}
-
-console.log(BubbleSort(arr));
---
+++ Linked List/linked-list-01.js
@@ -0,0 +1,55 @@
+//introduction to the linked list
+
+// step 1 : create node
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// step 2 : create the pointers in linked list
+// step 3 : write the functions in the linked list
+
+class LinkedList {
+  constructor() {
+    this.head = null; // pointer at first node of linked list
+    this.tail = null; // pointer at the last node of the linked list
+  }
+
+  add(data) {
+    let newnode = new Node(data); // naya node banao
+
+    if (this.head === null) {
+      this.head = newnode; // head ko naye node pe point karwao agar wo null pe set hai toh
+    } else {
+      this.tail.next = newnode; // warna tail jis node pe point karrha hai uske next se newnode ko point karwa do
+    }
+
+    this.tail = newnode; // finally, agar kuch nahi hai toh, tail ko newnode se point kardo
+  }
+
+
+  printAll(){
+    let current = this.head;  // naya node banao aur usko jahan head hai ussi node pe point karao
+
+    while(current !== null){
+        console.log(current.data);  // current node ka data print karo
+        current = current.next;  // aur pointer to next node pe move karo
+    }
+  }
+}
+
+
+// we can test it now
+
+const list = new LinkedList();
+
+list.add(10);
+list.add(20);
+list.add(30);
+list.add(40);
+list.add(50);
+
+list.printAll();
