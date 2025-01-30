44ab2b68b18be6c8d84871994776514b05bf6277
iamswatantra2000 <iamsearchingjobsfordev2025@gmail.com>
2025-01-30 09:45:20 +0530

    adding the files in SWT-02

--- Sliding-Window-Technique/SWT-02.js
+++ Sliding-Window-Technique/SWT-02.js
@@ -1,2 +1,13 @@
-// variable sized sliding window technique
+// variable length of sliding window
 
+function checkSubarrayVariable(arr, size) {
+    if (arr.length <= 0) return null;
+    if (size < 0 || size > arr.length) return null;
+
+
+
+}
+
+
+const arr = [2, 4, 1, 3, 7, 5, 6, 9, 8];
+const size = 3;
