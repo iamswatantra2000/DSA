8533a0a45b359779ecf162f9a904f3db6b2b02f0
Swatantra Gupta <iamswatantra2000@gmail.com>
2025-02-05 21:45:24 +0530

    adding the first cpp file

--- C++_Basics/cpp01.cpp
+++ C++_Basics/cpp01.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout << "Hello world!" << endl;
+
+class Employee
+{
+
+public:
+    Employee()
+    {
+        cout << "creating the constructor" << endl;
+    }
+
+    string name;
+    int age;
+    long phoneNo;
+    string address;
+};
+
+int main()
+{
+
+    Employee e1;
+    e1.name = "swatantra gupta";
+    e1.age = 21;
+    e1.phoneNo = 9084440850;
+    e1.address = "lohvan, mathura";
+
+    cout << e1.name << e1.age << e1.phoneNo << e1.address << endl;
+
     return 0;
 }
